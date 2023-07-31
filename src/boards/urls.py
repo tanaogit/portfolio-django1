84ad7f219a5c39f6afbde63bd8eb16b64537d2fa
Tanaka Naoya <nt3961185@gmail.com>
2023-07-31 22:41:55 +0900

    url修正

--- src/boards/urls.py
+++ src/boards/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'boards'
 
 urlpatterns = [
-    path('create_theme', views.create_theme, name = 'create_theme'),
-    path('list_themes', views.list_themes, name = 'list_themes'),
+    path('create_theme/', views.create_theme, name = 'create_theme'),
+    path('list_themes/', views.list_themes, name = 'list_themes'),
     path('edit_theme/<int:id>', views.edit_theme, name = 'edit_theme'),
     path('delete_theme/<int:id>', views.delete_theme, name = 'delete_theme'),
     path('post_comments/<int:theme_id>', views.post_comments, name = 'post_comments'),
-    path('save_comment', views.save_comment, name = 'save_comment'),
+    path('save_comment/', views.save_comment, name = 'save_comment'),
 ]
